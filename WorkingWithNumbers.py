6f43bc9222df8b7bd570c77567a5271f8f6057d2
texphos <66226570+texphos@users.noreply.github.com>
2021-01-18 12:23:34 -0500

    Add files via upload

---
+++ WorkingWithNumbers.py
@@ -0,0 +1,25 @@
+from math import *
+my_num = 7
+print(str(my_num) + " my favorite number")
+
+my_num = -5
+print(abs(my_num))
+
+my_num = -5
+print(pow(4,6))
+
+my_num = -5
+print(max(4,6))
+
+my_num = -5
+print(min(4,6))
+
+print(round(3.2))
+
+print(round(3.5))
+
+print(floor(3.5))
+
+print(ceil(3.5))
+
+print(sqrt(36))
